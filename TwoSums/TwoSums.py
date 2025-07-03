b7ce7920a86c2da6e1163f897be18157ebf4455a
AhmedMohamed50 <85414834+AhmedMohamed50@users.noreply.github.com>
2025-07-03 18:58:18 +0300

    leetcode pb1 another solution

--- TwoSums/TwoSums.py
+++ TwoSums/TwoSums.py
@@ -6,3 +6,21 @@ class Solution:
                     return [i,j]
         return []
         
+        
+        # another solution using dictionary
+        """
+        # Dictionary to store numbers and their corresponding indices
+        num_map = {}
+        
+        # Loop through each number in the array
+        for i, num in enumerate(nums):
+            complement = target - num
+            # If the complement is in the dictionary, return the indices
+            if complement in num_map:
+                return [num_map[complement], i]
+            # Otherwise, add the current number and its index to the dictionary
+            num_map[num] = i
+        
+        # If no solution found, return an empty list
+        return []
+        """
